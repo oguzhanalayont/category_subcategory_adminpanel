009abd2250c1f01656ac486251aaba1682c81081
oguzhanalayont <oguzhanalayont2021@gmail.com>
2024-08-02 11:26:25 +0300

    count added

--- app/Http/Controllers/CategoryViewController.php
+++ app/Http/Controllers/CategoryViewController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 use App\Models\Category;
+use App\Models\Forum;
 
 use Illuminate\Http\Request;
 
 class CategoryViewController extends Controller
 {
+
     public function view(Category $category)
     {
         $category->load('forums');
