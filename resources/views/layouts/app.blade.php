226f569eda2a1941ca4c03dea908d5beb59d8b29
oguzhanalayont <oguzhanalayont2021@gmail.com>
2024-10-27 18:42:21 +0300

    login register activated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Forum Project')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
@@ -26,12 +27,38 @@
                     </li>
                 </ul>
                 <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Login</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Register</a>
-                    </li>
+                    @guest
+                        @if (Route::has('login'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">Login</a>
+                            </li>
+                        @endif
+
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">Register</a>
+                            </li>
+                        @endif
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" 
+                               data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                   document.getElementById('logout-form').submit();">
+                                    Logout
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
                 </ul>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,49 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CategoryViewController;
 use App\Http\Controllers\ForumController;
 use App\Http\Controllers\ForumViewController;
 use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Auth;
 
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Ana sayfa rotası
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::resource('categories', CategoryController::class);
-Route::resource('forums', ForumController::class);
-Route::resource('posts', PostController::class);
+// Misafir kullanıcılar için rotalar
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/register', [AuthController::class, 'registerPost'])->name('register');
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/login', [AuthController::class, 'loginPost'])->name('login');
+});
 
-Route::get('/categories/{category}/view', [CategoryViewController::class, 'view'])->name('categories.view');
-Route::get('/forums/{forum}/view', [ForumViewController::class, 'view'])->name('forums.view');
-Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
-Route::get('/categories/{id}/forums', [ForumController::class, 'index'])->name('categories.forums');
-Route::get('/categories/show', [CategoryController::class, 'show'])->name('categories.show');
-Route::get('/categories/{category}/list', [CategoryController::class, 'list'])->name('categories.list');
+// Kimlik doğrulaması yapılmış kullanıcılar için rotalar
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/home', [HomeController::class, 'index']);
+    Route::delete('/logout', [AuthController::class, 'logout'])->name('logout');
+
+    // Resource rotaları
+    Route::resource('categories', CategoryController::class);
+    Route::resource('forums', ForumController::class);
+    Route::resource('posts', PostController::class);
+
+    // Özel görüntüleme rotaları
+    Route::get('/categories/{category}/view', [CategoryViewController::class, 'view'])->name('categories.view');
+    Route::get('/forums/{forum}/view', [ForumViewController::class, 'view'])->name('forums.view');
+    
+    // Kategori ile ilgili rotalar
+    Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
+    Route::get('/categories/{id}/forums', [ForumController::class, 'index'])->name('categories.forums');
+    Route::get('/categories/show', [CategoryController::class, 'show'])->name('categories.show');
+    Route::get('/categories/{category}/list', [CategoryController::class, 'list'])->name('categories.list');
+});
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
